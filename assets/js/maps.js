d18aefe1223c512358bf6125bf7521744c2ef57f
Birrellc <vaiezmedia@gmail.com>
2020-10-01 13:44:48 +0000

    test: api test

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,27 +1,8 @@
 function initMap() {
-    // Map options
-    var options = {
-        zoom: 15,
-        center: { lat: 54.971829446, lng: -1.608997564 }
-    }
 
-    // New map
-    var map = new google.maps.Map(document.getElementById('map'), options);
+    var eldonSquare = {lat: -25.344, lng: 131.036};
 
-    // Listen for click on map
-    google.maps.event.addListener(map, 'click', function (event) {
-        // Add marker
-        addMarker({ coords: event.latLng });
-    });
-
-
-    var marker = new google.maps.Marker({
-        position: { lat: 42.4668, lng: -70.9495 },
-        map: map,
-    });
-
-    marker.addListener('click', function () {
-        infoWindow.open(map, marker);
-    });
-
-};
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 10, center: eldonSquare});
+    var marker = new google.maps.Marker({position: eldonSquare, map: map});
+}
