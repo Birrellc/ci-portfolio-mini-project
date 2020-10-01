f6e48cecc9b0a10f33351176a59c2da943ab4b48
Birrellc <vaiezmedia@gmail.com>
2020-10-01 13:52:58 +0000

    test: api test

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,7 +1,7 @@
 function initMap() {
   const myLatLng = { lat: 54.971829446, lng: -1.608997564 };
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 8,
+    zoom: 10,
     center: myLatLng,
   });
   new google.maps.Marker({
