0d0500401e487cfbc11341be6be2a4439a8e0bb5
Birrellc <vaiezmedia@gmail.com>
2020-10-01 13:10:51 +0000

    test: test google maps api

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,9 +1,9 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 13,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 40.785091,
+            lng: -73.968285
         }
     });
 
@@ -21,6 +21,4 @@ function initMap() {
             label: labels[i % labels.length]
         });
     });
-
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
